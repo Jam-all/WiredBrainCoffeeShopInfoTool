dfd3e08e2aaa1e2a04963846bd5fb753c7568218
Kristof Heger <hegerkris@gmail.com>
2020-03-18 21:00:44 +0100

    Added new comment regarding testing

--- WiredBrainCoffee.ShopInfoTool/Program.cs
+++ WiredBrainCoffee.ShopInfoTool/Program.cs
@@ -7,6 +7,7 @@ namespace WiredBrainCoffee.ShopInfoTool
 		static void Main(string[] args)
 		{
 			Console.WriteLine("Wired Brain Coffee - Shop Info Tool!");
+			Console.WriteLine("This is a test of a merge of the new branch!");
 		}
 	}
 }
